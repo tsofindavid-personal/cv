efd803984b2e5dd503241b57006a5531818cca26
David Tsofin <tsofindavid@gmail.com>
2025-02-17 20:06:55 +0100

    update

--- push.sh
+++ push.sh
@@ -1,8 +1,8 @@
-pandoc README.md -o output.pdf \
+pandoc README.md -o david-tsofin-cv.pdf \
 --pdf-engine=xelatex \
 -V mainfont="Helvetica" \
 -V geometry:margin=0.8in
- 
+
 git add .
 git commit -m "update"
 git push
